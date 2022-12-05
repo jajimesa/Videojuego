d80946769affcf8fb1b90fe5a6b6a9c7e6f8acd0
sacamiso <sacamiso@unirioja.es>
2022-12-05 18:54:50 +0100

    SegundoVideo
    player movement

--- ProyectoUnity/Assets/PlayerMovement.cs
+++ ProyectoUnity/Assets/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        this.rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        this.rb.velocity = new Vector2(dirX * 7, this.rb.velocity.y);
+        if(Input.GetButtonDown("Jump"))
+        {
+            this.rb.velocity = new Vector2(this.rb.velocity.x, 14);
+        }
         
     }
 }
