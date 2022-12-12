70fdd014091245c8e0e4a29d72d548c237c06f75
sacamiso <sacamiso@unirioja.es>
2022-12-12 18:47:27 +0100

    Salto arreglado
    Salto perfectisiisisimo

--- ProyectoUnity/Assets/Scripts/PlayerMovement.cs
+++ ProyectoUnity/Assets/Scripts/PlayerMovement.cs
@@ -54,6 +54,9 @@ public class PlayerMovement : MonoBehaviour
     private enum MovementState { iddle, running, jumping, falling }
     private MovementState state;
 
+    private BoxCollider2D collider;
+    [SerializeField] private LayerMask jumpableGround;
+
 
     // Start es invocado en el primer frame de ejecución.
     private void Start()
@@ -62,9 +65,10 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update es invocado en cada nuevo frame .
+    // Update es invocado en cada nuevo frame.
     private void Update()
     {
         /* Input.GetAxis() e Input.GetAxisRaw() nos devuelve un input de estilo joystick,
@@ -79,7 +83,7 @@ public class PlayerMovement : MonoBehaviour
         
         /* Input.GetButtonDown cuenta ya con varios "botones" prefabricados, accedemos a ellos con el nombre que les da Unity en forma de string.
          */
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);     // Mantenemos la velocidad en el eje X si no queremos que frene en seco.
         }
@@ -120,5 +124,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anim.SetInteger("State", (int)state);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
